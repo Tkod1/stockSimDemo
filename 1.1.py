55de3fbdca897e00ced95d1e182fd0b49ab99c85
tkod1 <mbrrand@gmail.com>
2021-08-08 19:46:31 -0700

    First commit #100

---
+++ 1.1.py
@@ -0,0 +1,12 @@
+
+print('Hello Welcome to Stock Simulator.')
+print('what is your name?')
+userName = input()
+
+print('Alright ' + userName + ' how many days would you like to play? you can choose (1), (2), (3) through (10)')
+daysOfPlay = input()
+
+accountAmount = 300
+print(userName + ' You will start with an account amount of $300.00.')
+print('To simulate one day hit (d) + enter) for two days (dd), three (ddd).')
+userDaySkip = input()
